66a883ecabf391d03ec962146d40b681434c87ee
daniel <saucesteals@gmail.com>
2021-07-08 21:46:59 -0700

    images

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,10 +1,12 @@
+import ReactLogo from "@assets/icons/react.png";
+import TestSvg from "@assets/icons/test.svg";
 
 const HomePage = () => {
   return (
     <div className="flex bg-pink-300 h-screen w-screen justify-center items-center p-6">
       <h2>Hello from React!</h2>
-      {/* <img src={logo} />
-			<img src={TestSvg} /> */}
+      <img src={ReactLogo} width="100" />
+      <img src={TestSvg} width="100" />
     </div>
   );
 };
