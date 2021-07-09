577204675c555a07d61566e32882c3305e002e35
noah <noah.sigel@gmail.com>
2021-07-09 16:02:59 -0400

    feat(global): added redux demo
    BREAKING CHANGE: Event listeners may memory leak

--- src/render/redux/reducers/index.ts
+++ src/render/redux/reducers/index.ts
@@ -1,10 +1,21 @@
 import { combineReducers } from 'redux';
-const changeMe = () => {
-  return true;
-};
+
+function count(state: any, action: any) {
+  if (typeof state === 'undefined') {
+    state = 0; // If state is undefined, initialize it with a default value
+  }
+
+  if (action.type === 'INCREMENT') {
+    return state + 1;
+  } else if (action.type === 'DECREMENT') {
+    return state - 1;
+  } else {
+    return state; // In case an action is passed in we don't understand
+  }
+}
 
 const reducers = combineReducers({
-  changeMe,
+  count,
 });
 
 export default reducers;
