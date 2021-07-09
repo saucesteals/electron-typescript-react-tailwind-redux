a2ee16fe59f36532afceefd777e7aa2254b4ccf4
noah <noah.sigel@gmail.com>
2021-07-09 15:37:55 -0400

    feat(global): persistant changes

--- src/render/containers/HomePage/index.tsx
+++ src/render/containers/HomePage/index.tsx
@@ -1,7 +1,12 @@
+import { store } from '@store';
 import ReactLogo from '@assets/icons/react.png';
 import TestSvg from '@assets/icons/test.svg';
 
+import { useState } from 'react';
+
 const HomePage = () => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="flex bg-pink-300 h-screen w-screen justify-center items-center p-6 flex-col">
       <h2>Hello from React!</h2>
@@ -12,6 +17,8 @@ const HomePage = () => {
         <button onClick={window.electron.hide}>Hide</button>
         <button onClick={window.electron.relaunch}>Relaunch</button>
       </div>
+      <button onClick={() => setShow(!show)}>Show your store!</button>
+      {show ? <pre>{store.getState()}</pre> : null}
     </div>
   );
 };
