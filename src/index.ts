dd52bba1a4491cf25ff57c1172c7f4dae8015493
saucesteals <saucesteals@gmail.com>
2021-07-10 06:01:21 -0700

    docs(global): docs

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ const createWindow = (): void => {
 // Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
-createStores()
+createStores() // Make sure to call this ONCE.
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
