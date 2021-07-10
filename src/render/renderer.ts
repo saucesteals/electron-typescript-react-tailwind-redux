32447afd0a7746ea43b55ddedb336bcacf98411f
daniel <saucesteals@gmail.com>
2021-07-10 02:08:18 -0700

    docs(global): better docs

--- src/render/renderer.ts
+++ src/render/renderer.ts
@@ -4,26 +4,6 @@
  * Electron, visit:
  *
  * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
  */
 
 import ReactDOM from "react-dom";
@@ -35,5 +15,3 @@ function render() {
 }
 
 render();
-
-console.log('👋 This message is being logged by "renderer.ts", included via webpack');
