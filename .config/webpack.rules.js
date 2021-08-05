07ae51571f3f0c2b1cfd354e36ea61d6a4657099
noah <noah.sigel@gmail.com>
2021-08-05 19:54:59 -0400

    chore(global): updated webpack-asset-relocator-loader

--- .config/webpack.rules.js
+++ .config/webpack.rules.js
@@ -8,7 +8,7 @@ module.exports = [
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: "@marshallofsound/webpack-asset-relocator-loader",
+      loader: "@timfish/webpack-asset-relocator-loader",
       options: {
         outputAssetBase: "native_modules",
       },
