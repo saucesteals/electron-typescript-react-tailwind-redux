96c6f89524dd1ab08274da36592155195d148876
Daniel <saucesteals@gmail.com>
2021-08-08 23:32:24 -0700

    fix(global): replace native loader in rules

--- .config/webpack.rules.js
+++ .config/webpack.rules.js
@@ -8,7 +8,7 @@ module.exports = [
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: "@timfish/webpack-asset-relocator-loader",
+      loader: "@vercel/webpack-asset-relocator-loader",
       options: {
         outputAssetBase: "native_modules",
       },
