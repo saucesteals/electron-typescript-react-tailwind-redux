66e84a66e9ad57f8d405e6ab72e6187159bdaab6
Daniel <saucesteals@gmail.com>
2021-08-22 18:21:24 -0700

    fix(webpack): add correct ext

--- .config/webpack.rules.js
+++ .config/webpack.rules.js
@@ -48,7 +48,7 @@ module.exports = [
     test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
     type: "asset",
     generator: {
-      filename: "assets/[hash][ext]",
+      filename: "assets/[hash].[ext]",
     },
     parser: {
       dataUrlCondition: {
