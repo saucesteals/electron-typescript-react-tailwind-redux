59791bcfdfdf027298d87c0922d846d5bf396886
Daniel <saucesteals@gmail.com>
2022-03-04 20:53:10 -0800

    feat(render): use Navigate to redirect / to /Home

--- src/render/Routes.tsx
+++ src/render/Routes.tsx
@@ -2,6 +2,7 @@ import {
   Route,
   HashRouter as Router,
   Routes as Switch,
+  Navigate,
 } from "react-router-dom";
 import RouterPaths from "@/render/utils/routes.json";
 import HomePage from "@/render/containers/HomePage";
@@ -11,7 +12,11 @@ const Routes = () => {
     <>
       <Router>
         <Switch>
-          <Route caseSensitive path="/" element={<HomePage />} />
+          <Route
+            caseSensitive
+            path="/"
+            element={<Navigate replace to={RouterPaths.HOME} />}
+          />
           <Route caseSensitive path={RouterPaths.HOME} element={<HomePage />} />
         </Switch>
       </Router>
